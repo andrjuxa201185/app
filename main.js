9a4a9dbc44f8cb99359426a8193965a6924d7aa0
andrey podgorniy <andrey.podgorniy1985@gmail.com>
2019-03-27 00:06:05 +0200

    2 and 3 task done

---
+++ main.js
@@ -0,0 +1,9 @@
+const getRandomNum = require('./random');
+const read = require('./read');
+
+
+console.log(getRandomNum(44, 50));
+
+// read('name.txt')
+//     .then(text => console.log(text))
+//     .catch(err => console.log(err));
--- read.js
+++ read.js
@@ -1,12 +1,13 @@
 const fs = require('fs');
 
-const read = (path, callback) => fs.readFile(path, (error, data) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
+const read = (name) => {
+  return new Promise((res, rej) => {
+    fs.readFile(name, 'utf8', (err, data) => {
+       if (err) rej(err);
+       res(data.toString());
+    });
+  });
+}
 
-  callback(data.toString());
-});
-
+  
 module.exports = read;
