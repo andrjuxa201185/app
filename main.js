c9c632cd015bce57c2a76fff5075f02f0df00008
andrey podgorniy <andrey.podgorniy1985@gmail.com>
2019-03-26 22:31:59 +0200

    add packege

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const http = require('http');
 const weather = require('yahoo-weather');
 
 
-<<<<<<< HEAD
+
 // http.get('http://info.cern.ch', (res) => {
 //    if (res.statusCode !== 200) throw new Error(res.statusMessage);
 //    res.setEncoding('utf-8');
@@ -16,6 +16,5 @@ const getWeather = async () => {
 }
 
 getWeather();
-=======
-alert('hello')
->>>>>>> c44444bf0307211bbf57bbdcd6f7e51765a7ac1c
+
+  
