a842e4a3400668183f117b775b41d51bb8bae79f
andrey podgorniy <andrey.podgorniy1985@gmail.com>
2019-03-30 13:17:20 +0200

    add img

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
 const http = require('http');
+const fs = require('fs');
 
- const server = http.createServer((response, request) => {
-  request.end('Hello');
+const server = http.createServer((req, res) => {
+  if (req.url === '/git.png') {
+    fs.readFile('git.png', (err, data) => {
+      if (err) {
+          res.end('<strong>Error!</strong>');
+          throw new Error('Error while reading index.html');
+      };
+      res.end(data);
+    });
+
+    return;
+  }
+
+  res.setHeader('Content-Type', 'text/html');
+  fs.readFile('index.html', 'utf-8', (err, data) => {
+    if (err) {
+        res.end('<strong>Error!</strong>');
+        throw new Error('Error while reading index.html');
+    };
+    res.end(data);
+  });
+
 });
 
 server.listen(5555, () => {
   console.log('server has started');
 });
-
